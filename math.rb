424557198702686654468c6bbc31d16e9d84645a
soho <soho>
2019-01-03 13:48:40 +0000

    completed Ch. 5 extension - higher math

--- math.rb
+++ math.rb
@@ -1,1 +1,17 @@
 # CHAPTER 5 - EXTENSION: HIGHER MATHS
+puts rand
+puts rand (12357)
+srand 5
+puts rand (101)
+puts rand (5.256)
+puts rand (101)
+puts rand (101)
+puts rand (101)
+
+puts rand (101)
+srand 5
+srand 5
+srand 5
+puts
+puts (Math::PI)
+puts (Math::E)
